f70b0f2513538c1b87c06447e660942f521c0a2a
Adam Miedema <armgitaar@yahoo.com>
2020-08-18 15:30:23 -0600

    wip

--- config/database.js
+++ config/database.js
@@ -45,13 +45,13 @@ module.exports = {
   |
   */
   mysql: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      host: Env.get('DB_HOST'),
+      port: Env.get('DB_PORT'),
+      user: Env.get('DB_USER'),
+      password: Env.get('DB_PASSWORD'),
+      database: Env.get('DB_NAME')
     }
   },
 
